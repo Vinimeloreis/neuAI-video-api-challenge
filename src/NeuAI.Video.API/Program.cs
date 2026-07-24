5505cc814a08611f8cc7403cf48ed9d355ef188f
Vinimeloreis <viniciusmeloreisdonascimento@gmail.com>
2026-07-23 21:09:15 -0300

    feat(program.cs appsettings.json): redis + builder + caminho no appsettings.json
    adicionando a conexão singleton com o redis e registrando ele no builder com a rota especificada no
    appsettings.json por motivos de escalabilidade

--- src/NeuAI.Video.API/Program.cs
+++ src/NeuAI.Video.API/Program.cs
@@ -1,10 +1,11 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("Redis")));
 builder.Services.AddControllers();
 
 var app = builder.Build();
-
 
 app.UseHttpsRedirection();
 
